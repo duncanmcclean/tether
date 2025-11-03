8ed3e16caa494447e98841219163bfbd6cbf97c4
Duncan McClean <duncan@duncanmcclean.com>
2025-11-03 14:26:40 +0000

    Add '$HOME/Sites' to search directories

--- tether.sh
+++ tether.sh
@@ -26,7 +26,7 @@ fi
 PACKAGE="$1"
 
 CODE_DIRECTORY=""
-for dir in "$HOME/Code" "$HOME/Herd" "$HOME/Valet"; do
+for dir in "$HOME/Code" "$HOME/Herd" "$HOME/Valet" "$HOME/Sites"; do
     if [[ -d "$dir" ]]; then
         CODE_DIRECTORY="$dir"
         break
